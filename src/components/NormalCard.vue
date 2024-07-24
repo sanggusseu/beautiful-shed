162e84d60974d73aa9068372eeaefbe7a5fcd298
sanggusseu <sanggusseu@gmail.com>
2024-07-25 00:19:12 +0900

    NormalCard 컴포넌트 설정

---
+++ src/components/NormalCard.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="col-sm-12 col-md-4 col-lg-3">
+		<div class="card h-100">
+			<div class="card-body d-flex flex-column">
+				<h5 class="card-title">{{ title }}</h5>
+				<p class="card-text flex-grow-1">{{ text }}</p>
+				<a :href="url" target="_blank" class="btn btn-primary">방문하기</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+	url: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>
