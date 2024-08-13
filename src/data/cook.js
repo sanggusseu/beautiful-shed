9e14bd854f6075d5172e331b44bee73f21084f5e
sanggusseu <sanggusseu@gmail.com>
2024-08-13 17:31:24 +0900

    요리 레시피 추가

---
+++ src/data/cook.js
@@ -0,0 +1,18 @@
+const recipes = [
+	{
+		type: '전자레인지',
+		title: '전자레인지 계란찜',
+		text: '음식점 계란찜 맛 나는 초간단 전자레인지 계란찜',
+		url: 'https://www.youtube.com/watch?v=86chKdRUVSg',
+	},
+	{
+		type: '냄비',
+		title: '인생간장국수',
+		text: '진짜 인생간장국수 👍',
+		url: 'https://www.youtube.com/shorts/bI0ceoR9TtQ',
+	},
+];
+
+export const fetchRecipes = () => {
+	return recipes;
+};
--- src/views/CookView.vue
+++ src/views/CookView.vue
@@ -1,13 +1,26 @@
 <template>
 	<SectionRow>
-		<SectionTitle>요리 사이트</SectionTitle>
-		<p>준비중입니동</p>
+		<SectionTitle>🍳 매일 먹어도 안 질리는 레시피 모음</SectionTitle>
+		<FeaturedCard
+			v-for="(recipe, i) in recipes"
+			:key="i"
+			:type="recipe.type"
+			:title="recipe.title"
+			:text="recipe.text"
+			:url="recipe.url"
+		>
+		</FeaturedCard>
 	</SectionRow>
 </template>
 
 <script setup>
+import FeaturedCard from '@/components/FeaturedCard.vue';
 import SectionRow from '@/components/SectionRow.vue';
 import SectionTitle from '@/components/SectionTitle.vue';
+import { fetchRecipes } from '@/data/cook';
+import { ref } from 'vue';
+const recipes = ref([]);
+recipes.value = fetchRecipes();
 </script>
 
 <style lang="scss" scoped></style>
